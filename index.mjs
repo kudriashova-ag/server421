6d4bc3bf8d151d80cb6b43b7b49b70e0b57866f2
Anastasiia Kudriashova <kudriashova.ag@gmail.com>
2025-03-23 20:51:00 +0200

    Server end

--- index.mjs
+++ index.mjs
@@ -2,6 +2,13 @@ import express from 'express'
 import dbConnection from './db/db.mjs'
 import cors from 'cors'
 import { createTask, deleteTask, getTasks, updateTask } from './controllers/taskController.mjs'
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 
 const app = express()
 const port = 5000
@@ -11,10 +18,7 @@ dbConnection.on('error', () => console.log('db connected error'));
 
 app.use(express.json())
 app.use(cors())
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.use(express.static('build'))
 
 
 app.get('/api/tasks', getTasks)
@@ -22,6 +26,10 @@ app.post('/api/tasks', createTask)
 app.put('/api/tasks/:id', updateTask)
 app.delete('/api/tasks/:id', deleteTask)
 
+
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/build/index.html')
+})
 
 
 app.listen(port, () => {
